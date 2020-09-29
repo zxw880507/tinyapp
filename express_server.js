d3ea0da99230f2337a68e49af8b7ca6f15fe7bee
Xiaowen Zheng <zxw880507@gmail.com>
2020-09-29 22:56:48 +0000

    logout completed

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const generateRandomString = () => {
     const randomChar = () => {
         return String.fromCharCode(Math.floor(Math.random() * 75) + 48);
@@ -14,7 +15,7 @@ const generateRandomString = () => {
     return str;
 };
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.set('view engine', "ejs");
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,11 +31,12 @@ app.get(`/hello`, (req, res) => {
     res.send(`<html><body>Hello <b>World</b></body></html>\n`);
 });
 app.get('/urls', (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
     res.render('urls_index', templateVars);
 });
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    const templateVars = { username: req.cookies['username'] };
+    res.render("urls_new", templateVars);
 });
 app.post('/urls', (req, res) => {
     const shortURL = generateRandomString();
@@ -46,7 +48,7 @@ app.get('/u/:shortURL', (req, res) => {
     res.redirect(longURL);
 });
 app.get('/urls/:shortURL', (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username'] };
     res.render('urls_show', templateVars);
 });
 app.post('/urls/:shortURL', (req, res) => {
@@ -58,6 +60,15 @@ app.post(`/urls/:shortURL/delete`, (req, res) => {
     delete urlDatabase[req.params.shortURL];
     res.redirect('/urls');
 });
+app.post('/login', (req, res) => {
+    const cookies = req.body.username;
+    res.cookie('username', cookies, { expires: new Date(+new Date() + 900000), httpOnly: true });
+    res.redirect('/urls');
+});
+app.post(`/logout`, (req, res) => {
+    res.clearCookie('username');
+    res.redirect(`/urls`);
+})
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
